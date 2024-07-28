769f46178d89572660451ed3f5abe7a138452b37
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-07-28 20:25:39 +0700

    video33

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,17 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Hello World ";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user ";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin";
     }
 
 }
