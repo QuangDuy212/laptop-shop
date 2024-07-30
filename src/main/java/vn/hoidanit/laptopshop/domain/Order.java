68ce4b8a97c00da1dfae985593fbebeacb1ee37a
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-07-30 15:14:38 +0700

    video79

--- src/main/java/vn/hoidanit/laptopshop/domain/Order.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Order.java
@@ -1,9 +1,13 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,6 +17,11 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private double totalPrice;
+
+    // Order -> ManyToOne -> User
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public long getId() {
         return id;
