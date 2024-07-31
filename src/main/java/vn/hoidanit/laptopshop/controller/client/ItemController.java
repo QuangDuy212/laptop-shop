e965ac4b6afc2227066d943ea159350512580800
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-07-31 15:48:35 +0700

    video97

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -0,0 +1,18 @@
+package vn.hoidanit.laptopshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import vn.hoidanit.laptopshop.domain.User;
+
+@Controller
+public class ItemController {
+    @GetMapping("/product/{id}")
+    public String getProductDetailPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        model.addAttribute("newUser", new User());
+        return "client/product/detail";
+    }
+}
