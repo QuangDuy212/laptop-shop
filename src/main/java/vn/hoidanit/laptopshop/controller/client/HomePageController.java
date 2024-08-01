9db8605147fcf6863f8e127329a79f60828f2ffe
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-01 20:00:26 +0700

    video108

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -5,9 +5,16 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
 import vn.hoidanit.laptopshop.service.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class HomePageController {
@@ -26,6 +33,14 @@ public class HomePageController {
 
     @GetMapping("/register")
     public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
         return "client/auth/register";
     }
+
+    @PostMapping("/register")
+    public String postMethodName(Model model, @ModelAttribute("registerUser") RegisterDTO registerDTO) {
+
+        return "client/auth/register";
+    }
+
 }
