2d9857a217bc434fb527a5002a7ddb3a24e8d4c5
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-02 20:01:03 +0700

    video119

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
@@ -42,15 +43,29 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public AuthenticationSuccessHandler customSuccessHandler() {
+        return new CustomSuccessHandler();
+    }
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE)
                         .permitAll()
-                        .requestMatchers("/", "/login", "/client/**", "/css/**", "/js/**", "/images/**", "/register")
+
+                        .requestMatchers("/", "/login", "/client/**", "/css/**", "/js/**", "/images/**", "/register",
+                                "/product/**")
                         .permitAll()
+
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+
                         .anyRequest().authenticated())
-                .formLogin(formLogin -> formLogin.loginPage("/login").failureUrl("/login?error").permitAll());
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .failureUrl("/login?error")
+                        .successHandler(customSuccessHandler())
+                        .permitAll());
         return http.build();
     }
 }
