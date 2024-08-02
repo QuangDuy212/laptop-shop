fb83bbed7b09d258f72c01733eb2a4b78bf07d0d
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-02 15:11:48 +0700

    video115

--- src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
@@ -10,6 +10,7 @@ public class RegisterDTO {
     private String firstName;
     private String lastName;
 
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
     private String password;
 
